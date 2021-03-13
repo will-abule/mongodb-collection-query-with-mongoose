cd6193297575bf2d1c4456f42cdc998121a2f7c3
Will Abule <willxd42@gmail.com>
2021-03-13 12:08:09 +0100

    added typescript types

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,3 @@
 import { Model, Document } from "mongoose";
 import { QueryInterface, Response as Res } from "./utils/interfaces-utils";
-
-export { QueryInterface };
-
-export default function getResReq(
-  query: QueryInterface,
-  DBModel: Model<Document>,
-  select: string
-): Promise<Res>;
+export default function getResReq(query: QueryInterface, DBModel: Model<Document>, select: string): Promise<Res>;
