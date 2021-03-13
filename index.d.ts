405029c79c36f259d6f4109e33a9c7d1f87c0acd
Will Abule <willxd42@gmail.com>
2021-03-13 12:09:47 +0100

    made imports cleaner

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,10 @@
 import { Model, Document } from "mongoose";
 import { QueryInterface, Response as Res } from "./utils/interfaces-utils";
-export default function getResReq(query: QueryInterface, DBModel: Model<Document>, select: string): Promise<Res>;
+
+export { QueryInterface };
+
+export default function getResReq(
+  query: QueryInterface,
+  DBModel: Model<Document>,
+  select: string
+): Promise<Res>;
