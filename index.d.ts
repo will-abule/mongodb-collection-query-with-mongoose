515839af7e7c23d41d95d7160a467880aae13642
will-abule <willxd42@gmail.com>
2021-12-09 06:43:30 +0100

    changed entry point

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import { Model, Document } from "mongoose";
+import { QueryInterface, Response as Res } from "./utils/interfaces-utils";
+export default function getResReq(query: QueryInterface, DBModel: Model<Document>, select: string): Promise<Res>;
